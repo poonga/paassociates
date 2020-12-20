f1eb9e64c26d952e46b230f96f4f4421a72ba0c0
poonkodi <ipoonkodibe@gmail.com>
2020-12-20 22:02:27 +0530

    changes logo.

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -183,7 +183,7 @@
                 },
                
                 xAxis: [{
-                    categories: users,
+                    categories: ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10', 'user11'],
                     crosshair: true
                 }],
                 yAxis: [{ // Primary yAxis
@@ -232,7 +232,7 @@
                     name: 'Projects',
                     type: 'column',
                     yAxis: 1,
-                    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+                    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6],
                     tooltip: {
                         valueSuffix: ' mm'
                     }
@@ -240,7 +240,7 @@
                 }, {
                     name: 'Payments',
                     type: 'spline',
-                    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+                    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9],
                     tooltip: {
                         valueSuffix: '°C'
                     }
